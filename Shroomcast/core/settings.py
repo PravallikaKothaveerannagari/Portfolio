924b867d27d9d7ba3aed5a81d42362312598424f
PravallikaKV <113497539+PravallikaKothaveerannagari@users.noreply.github.com>
2024-03-23 22:32:25 +0530

    Update settings.py

--- Shroomcast/core/settings.py
+++ Shroomcast/core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-(ic==0&v1g5#9lggdaeevm=jova$n0%czpu7oby1-fe^d1kgfk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
